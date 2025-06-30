3b85105aa6c8911182f0d25e89b678809ef9a3bd
Dennis-Andrade <denisandradeg@gmail.com>
2025-06-30 09:56:59 -0500

    Add files via upload
    Primera version de la libreria del Intesud donde se creo los primeros paquetes .controller .model .repository y sus archivos JAVA

---
+++ Llibreria 1.0/src/main/java/com/libreria/Llibreria/repository/LibroRepository.java
@@ -0,0 +1,7 @@
+package com.libreria.Llibreria.repository;
+
+import com.libreria.Llibreria.model.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibroRepository extends JpaRepository<Libro, Long> {
+}
