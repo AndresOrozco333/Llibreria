878bd555bebe2c3c6893f2b889d0050904aae176
Dennis-Andrade <denisandradeg@gmail.com>
2025-07-06 21:57:41 -0500

    Primera Version Basica de la interfaz grafica
    Version 0.5 de la interfaz grafica para L-Libreria dado que la version anterior tenia demasiados errores se decido empezar de cero para lograr el objetivo de la interfaz grafica esta vez usando html y un poco de java

--- Llibreria 1.0/src/main/java/com/libreria/Llibreria/repository/LibroRepository.java
+++
@@ -1,7 +0,0 @@
-package com.libreria.Llibreria.repository;
-
-import com.libreria.Llibreria.model.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepository extends JpaRepository<Libro, Long> {
-}
