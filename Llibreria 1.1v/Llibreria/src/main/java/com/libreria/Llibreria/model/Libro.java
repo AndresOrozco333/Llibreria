bd06f5abe6ea9854ace2ed465ee15e5bf5b2bd58
AndresOrozco333 <cristhopherorozco33@gmail.com>
2025-07-06 23:12:19 -0500

    Add files via upload

---
+++ Llibreria 1.1v/Llibreria/src/main/java/com/libreria/Llibreria/model/Libro.java
@@ -0,0 +1,27 @@
+package com.libreria.Llibreria.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String titulo;
+    private String autor;
+    private String categoria;
+
+    // Getters y setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getTitulo() { return titulo; }
+    public void setTitulo(String titulo) { this.titulo = titulo; }
+
+    public String getAutor() { return autor; }
+    public void setAutor(String autor) { this.autor = autor; }
+
+    public String getCategoria() { return categoria; }
+    public void setCategoria(String categoria) { this.categoria = categoria; }
+}
