9700256c1334cf67e59ae5be706fd260b4b68692
AndresOrozco333 <cristhopherorozco33@gmail.com>
2025-07-28 08:32:25 -0500

    Delete Llibreria v0.9 directory

--- Llibreria v0.9/src/main/java/com/libreria/Llibreria/repository/LibroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.libreria.Llibreria.repository;
-
-import com.libreria.Llibreria.model.Libro;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LibroRepository extends JpaRepository<Libro, Long> {
-    // Aquí  definir métodos personalizados
-}
