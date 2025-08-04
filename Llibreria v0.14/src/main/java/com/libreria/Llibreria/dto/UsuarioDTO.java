6133b854f1bb837a7bd31bac208a358d87ef953d
Dennis-Andrade <denisandradeg@gmail.com>
2025-08-04 09:31:57 -0500

    Creacion de los registros
    Se creo un nuevo apartado para registrar nuevos usuarios en la plataforma con su respectivo rol obviamente se descarto el de admin por temas de seguridad

---
+++ Llibreria v0.14/src/main/java/com/libreria/Llibreria/dto/UsuarioDTO.java
@@ -0,0 +1,23 @@
+package com.libreria.Llibreria.dto;
+
+public class UsuarioDTO {
+
+    private String username;
+    private String password;
+    private String nombre;
+    private String rol;
+
+    public UsuarioDTO() {}
+
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public String getRol() { return rol; }
+    public void setRol(String rol) { this.rol = rol; }
+}
