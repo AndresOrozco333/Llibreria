a162a3610ab1dee4860d90d31846e9f45b1c6f88
AndresOrozco333 <cristhopherorozco33@gmail.com>
2025-08-11 10:12:00 -0500

    Delete Llibreria v0.12 directory

--- Llibreria v0.12/src/main/java/com/libreria/Llibreria/repository/LibroRepository.java
+++
@@ -1,8 +0,0 @@
-package com.libreria.Llibreria.repository;
-
-import com.libreria.Llibreria.model.Libro;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface LibroRepository extends MongoRepository<Libro, String> {
-    // Puedes agregar métodos personalizados si los necesitas
-}
